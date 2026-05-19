a98a423972c8cf73417753a8af20c9e1d88aac3f
igt1257 <igt1257@yahoo.com.tw>
2026-05-19 17:15:08 +0800

    debug 1

--- app.py
+++ app.py
@@ -232,11 +232,7 @@ def index():
 if __name__ == "__main__":
 
     start_loader()
-
-    port = int(os.environ.get("PORT", 5000))
-
     app.run(
         host="0.0.0.0",
         port=port,
-        debug=False
     )
